b92c7766a0bfab36938f35fd226ae6808068482c
JordanEZRA <jordanezravlogs@gmail.com>
2025-03-30 11:26:27 +0800

    Update love.py

--- love.py
+++ love.py
@@ -1,4 +1,4 @@
-with open("lablab.html", "w", encoding="utf-8") as file:
+with open("index.html", "w", encoding="utf-8") as file:
     file.write(html_content)
 
 print("HTML file generated! Open 'love_message.html' in your browser.")
